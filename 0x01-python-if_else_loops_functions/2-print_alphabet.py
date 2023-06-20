5ab11eafae8d9b8c7471bec3c28f78a1fc977407
DaffiX <suleydaffy@gmail.com>
2023-06-20 06:40:25 +0300

    ASCII char - chr()

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# a program that prints ASCII alphabet in lowercase
+# chr - a function used to convert ascii values to their corresponding characters.
+for ch in range(97, 122):
+    print(chr(ch),end="") 
