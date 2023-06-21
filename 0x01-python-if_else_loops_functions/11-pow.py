ce7161f3d05c7a48d2f888ea33b65d8407d427f6
DaffiX <suleydaffy@gmail.com>
2023-06-21 08:25:49 +0300

    Power of a number

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# function that compute power of a num
+
+def pow(a, b):
+    return a ** b
+
+
+print(pow(2,2))
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
