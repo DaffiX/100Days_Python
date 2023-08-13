00ce951325c111241e7c5459e7568266f989e4ca
DaffiX <suleydaffy@gmail.com>
2023-08-13 17:31:20 +0300

    Pagination

---
+++ 0x16-python_api/2-swapi_pagination.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""some endpoint in SWAPI support pagination. The response will contain `next` field if more data available. USE LOOP to fetch all data
+"""
+import requests 
+
+base_url = "https://swapi.dev/api"
+endpoint = "people/"
+url = f"{base_url}/{endpoint}"
+
+while url:
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+
+        for character in data["results"]: # swapi keyword
+            print(character["name"])
+
+        url = data["next"] # move to next page of result
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+        break
