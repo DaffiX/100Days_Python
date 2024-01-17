c94c602a3077fb970a492d3b3b8a0a0a08791602
YUNUS <105539783+snavid@users.noreply.github.com>
2024-01-18 00:28:52 +0300

    aded code like a pro codes

--- python_basic/0-majority_element.py
+++ python_basic/0-majority_element.py
@@ -26,10 +26,22 @@ def find_majority_elemet(lst):
 
 # test the function
 numbers = [1, 7, 8, 7, 7, 7]
-majority_element = find_majority_elemet(numbers)
+
+    
+'''code like a pro'''
+from collections import Counter
+
+def find_majority_elements(lst):
+    counts = Counter(lst)
+    print(counts)
+    majority_element = max(counts.keys(), key=counts.get, default=None)
+    return majority_element if counts[majority_element] >  2 else None  
+    
+    
+    
+majority_element = find_majority_elements(numbers)
 
 if majority_element is not None:
     print(f"The majority element is  {majority_element}")
 else:
     print("There is no majority element in the list provided")
-    
